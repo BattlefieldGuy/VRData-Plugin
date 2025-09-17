5d5c0103cb7da6b8cb86feb69aa2c2ada24af81d
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-09-17 15:06:14 +0200

    Full test demo with input

--- Assets/Devs/Matt/Scripts/DisplayInputData.cs
+++ Assets/Devs/Matt/Scripts/DisplayInputData.cs
@@ -4,8 +4,23 @@ using UnityEngine.XR;
 
 public class DisplayInputData : MonoBehaviour
 {
-    public TextMeshProUGUI LeftScoreDisplay;
-    public TextMeshProUGUI RightScoreDisplay;
+    [Header("LeftSide")]
+    public TextMeshProUGUI PositionL;
+    public TextMeshProUGUI RotationL;
+    public TextMeshProUGUI GripL;
+    public TextMeshProUGUI TriggerL;
+    public TextMeshProUGUI JoystickL;
+    public TextMeshProUGUI YButton;
+    public TextMeshProUGUI XButton;
+
+    [Header("RightSide")]
+    public TextMeshProUGUI PositionR;
+    public TextMeshProUGUI RotationR;
+    public TextMeshProUGUI GripR;
+    public TextMeshProUGUI TriggerR;
+    public TextMeshProUGUI JoystickR;
+    public TextMeshProUGUI BButton;
+    public TextMeshProUGUI AButton;
 
     private InputData inputData;
     private float leftMaxScore = 0f;
@@ -18,15 +33,52 @@ public class DisplayInputData : MonoBehaviour
 
     void Update()
     {
-        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 leftVelocity))
-        {
-            leftMaxScore = Mathf.Max(leftVelocity.magnitude, leftMaxScore);
-            LeftScoreDisplay.text = leftMaxScore.ToString();
-        }
-        if (inputData.RightController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 rightVelocity))
-        {
-            rightMaxScore = Mathf.Max(rightVelocity.magnitude, rightMaxScore);
-            LeftScoreDisplay.text = "" + leftMaxScore.ToString();
-        }
+        //LeftSide
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 positionL))
+            PositionL.text = "Position: " + positionL.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotationL))
+            RotationL.text = "Rotation: " + rotationL.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.grip, out float gripL))
+            GripL.text = "Grip value: " + gripL.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.trigger, out float triggerL))
+            TriggerL.text = "Trigger value: " + triggerL.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystickL))
+            JoystickL.text = "Joystick value: " + joystickL.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool Ybutton))
+            YButton.text = "Y button: " + Ybutton.ToString();
+
+        if (inputData.LeftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Xbutton))
+            XButton.text = "X button: " + Xbutton.ToString();
+
+        //RightSide
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 positionR))
+            PositionR.text = "Position: " + positionR.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotationR))
+            RotationR.text = "Rotation: " + rotationR.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.grip, out float gripR))
+            GripR.text = "Grip value: " + gripR.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.trigger, out float triggerR))
+            TriggerR.text = "Trigger value: " + triggerR.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystickR))
+            JoystickR.text = "Joystick value: " + joystickR.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool Bbutton))
+            BButton.text = "B button: " + Bbutton.ToString();
+
+        if (inputData.RightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool Abutton))
+            AButton.text = "A button: " + Abutton.ToString();
+
+
+
+        //if (inputData.HMD.TryGetFeatureUsages(CommonUsages.))
     }
 }
